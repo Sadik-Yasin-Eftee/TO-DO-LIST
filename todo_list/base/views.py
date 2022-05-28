fd945f6012b1454d66113bd7ee59a8ea24c8e981
Sadik-Yasin-Eftee <sadikyasin492@gmail.com>
2022-05-29 01:36:03 +0600

    Create Item Section Added

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,8 +1,12 @@
+from turtle import title
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
 from .models import Task
+
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 class TaskList(ListView):
     model = Task
@@ -12,3 +16,8 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks') #redirects the page to the initial page after adding a task
